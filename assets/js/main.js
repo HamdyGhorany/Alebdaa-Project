437a85a4c4c3ea353c42ba1292dfa3a7a0e85a9b
HamdyGhorany <hamdy.ghorany@gmail.com>
2020-09-21 16:56:03 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -201,3 +201,18 @@
   });
 
 })(jQuery);
+
+
+
+
+var swiper = new Swiper('.swiper-container', {
+  effect: 'flip',
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
